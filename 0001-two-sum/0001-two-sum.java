bd3f8a9a43705433fb0a126be72b2e5aa190ade1
TechSenseiX <rishitarani406@gmail.com>
2024-05-31 21:49:42 +0530

    Time: 2 ms (97.71%), Space: 44.8 MB (36.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,62 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i =0; i< nums.length; i++){
-          for(int j=i+1; j<nums.length;j++){
-          if(nums[i] + nums[j] == target){
-            return new int[]{i,j};
-          }
+     Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Calculate the complement
+            int complement = target - nums[i];
+            
+            // Check if the complement exists in the map
+            if (map.containsKey(complement)) {
+                // Return the indices of the two numbers
+                return new int[] { map.get(complement), i };
+            }
+            
+            // Add the current number and its index to the map
+            map.put(nums[i], i);
         }
-          
-        }
-      
-      return null;
-      
+        return new int[0];
+    
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //here we can not sort the array because we have to return the index
+//         int ans[] = new int[2];
+//         int n = nums.length;
+//         Arrays.sort(nums);
+//         //two pointer approach
+//         int start = 0;
+//         int end = n-1;
+
+//         while(start < end){
+//             int sum= nums[start]+nums[end];
+//             if(sum==target){
+//                 ans[0]=start;
+//                 ans[1]=end;
+//             }
+//            else if(sum<target){
+//                  start++;
+//             }
+//           else {
+//             end--;
+//           }
+//         }
+        
+//       return ans;
+      
+//     }
+// }
